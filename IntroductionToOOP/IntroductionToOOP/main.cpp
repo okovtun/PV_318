cbef2177945f9411e22178d05eadb4070bda09b3
Clayman <Clayman@DESKTOP-DM8E77U>
2024-05-29 21:38:30 +0300

    Operators overloading

--- IntroductionToOOP/IntroductionToOOP/main.cpp
+++ IntroductionToOOP/IntroductionToOOP/main.cpp
@@ -69,6 +69,20 @@ public:
 		return *this;
 	}
 
+	Point& operator++()	//Prefix increment
+	{
+		x++;
+		y++;
+		return *this;
+	}
+	Point operator++(int)//Posfix increment
+	{
+		Point old = *this;
+		x++;
+		y++;
+		return old;
+	}
+
 	//				Methods:
 	double distance(const Point& other)const
 	{
@@ -98,10 +112,43 @@ double distance(const Point& A, const Point& B)
 	//Функция sqrt() возвращает квадратный корень принятого числа
 }
 
+Point operator+(const Point& left, const Point& right)
+{
+	Point result;
+	result.set_x(left.get_x() + right.get_x());
+	result.set_y(left.get_y() + right.get_y());
+	return result;
+}
+Point operator-(const Point& left, const Point& right)
+{
+	Point result
+	(
+		left.get_x() - right.get_x(), 
+		left.get_y() - right.get_y()
+	);
+	return result;
+}
+Point operator*(const Point& left, const Point& right)
+{
+	return Point
+	(
+		left.get_x()*right.get_x(),
+		left.get_y()*right.get_y()
+	);
+}
+bool operator==(const Point& left, const Point& right)
+{
+	return left.get_x() == right.get_x() && left.get_y() == right.get_y();
+	/*if (left.get_x() == right.get_x() && left.get_y() == right.get_y())
+		return true;
+	else return false;*/
+}
+
 //#define STRUCT_POINT
 //#define DISTANCE_CHECK
 //#define CONSTRUCTORS_CHECK
 //#define ASSIGNMENT_CHECK
+//#define ARITHMETICAL_OPERATORS_CHECK
 
 void main()
 {
@@ -185,11 +232,25 @@ void main()
 	C.print();
 #endif // ASSIGNMENT_CHECK
 
+#ifdef ARITHMETICAL_OPERATORS_CHECK
+	int a = 2;
+	int b = 3;
+	int c = a - b;
+
 	Point A(2, 3);
 	Point B(7, 8);
-	Point C = A + B;
+	A.print();
+	B.print();
+
+	Point C = A * B;
 	C.print();
 
+	C++;
+	C.print();
+#endif // ARITHMETICAL_OPERATORS_CHECK
+
+	//cout << (2 == 3) << endl;
+	cout << (Point(2, 3) == Point(3, 3)) << endl;
 }
 
 /*
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,10 +1,13 @@
 https://github.com/okovtun/PV_318.git
 https://www.youtube.com/playlist?list=PLeqyOOqxeiIPt4Opfdrc50pI9PbPSjOaV
+https://meet.google.com/mho-khdw-fhj
 
 TODO:
 0. Выучить теорию!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 1. https://learn.microsoft.com/en-us/cpp/cpp/operator-overloading?view=msvc-170
 2. https://learn.microsoft.com/en-us/cpp/cpp/general-rules-for-operator-overloading?view=msvc-170
+3. В Solution 'IntroductionToOOP' добавить проект 'Fraction',
+   и в нем реализовать класс 'Fraction', описывающий простую дробь;
 
 DONE:
 1. Почему при вызове метода distance() и функции distance() копируются объекты;
