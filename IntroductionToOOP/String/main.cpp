5c1b05e23ecfd6dd78faac69f226e258e86d6d17
User <User@DESKTOP-5PSJK0D>
2024-06-17 19:48:45 +0300

    Header initialization DONE

--- IntroductionToOOP/String/main.cpp
+++ IntroductionToOOP/String/main.cpp
@@ -22,24 +22,24 @@ public:
 
 
 	//			Constructors:
-	explicit String(int size = 80)
+	explicit String(int size = 80) :size(size), str(new char[size] {})
 	{
-		this->size = size;
-		this->str = new char[size] {};
+		//this->size = size;
+		//this->str = new char[size] {};
 		cout << "DefaultConstructor:\t" << this << endl;
 	}
-	String(const char str[])
+	String(const char str[]) :size(strlen(str) + 1), str(new char[size] {})
 	{
-		this->size = strlen(str) + 1;
-		this->str = new char[size] {};
+		//this->size = strlen(str) + 1;
+		//this->str = new char[size] {};
 		for (int i = 0; str[i]; i++)this->str[i] = str[i];
 		cout << "Constructor:\t\t" << this << endl;
 	}
-	String(const String& other)
+	String(const String& other) :size(other.size), str(new char[size] {})
 	{
 		//Deep copy
-		this->size = other.size;
-		this->str = new char[size] {};
+		//this->size = other.size;
+		//this->str = new char[size] {};
 		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
 		cout << "CopyConstructor:\t" << this << endl;
 	}
@@ -89,13 +89,41 @@ String operator+(const String& left, const String& right)
 	return buffer;
 }
 
+#define CONSTRUCTORS_CHECK
+//#define CAT_CHECK
+
 void main()
 {
 	setlocale(LC_ALL, "");
-	//String str;
+
+#ifdef CONSTRUCTORS_CHECK
+	String str1;			//Default constructor
+	str1.print();
+
+	String str2(8);
+	str2.print();			//Single-Argument constructor
+
+	String str3 = "Hello";	//Single-Argument constructor
+	str3.print();
+
+	String str4();			//Здесь НЕ вызывается конструктор и НЕ создается объект, 
+							//в этом выражении объявляется функция str4(),
+							//которая ничего не принимает, и возвращает объект класса 'String'.
+	//str4.print();
+
+	String str5{};			//Явный вызов конструктора по умолчанию
+	str5.print();
+
+	String str6{ str3 };	//Copy constructor
+	str6.print();
+#endif // CONSTRUCTORS_CHECK
+
+
+#ifdef CAT_CHECK
+		//String str;
 	//str.print();
 
-	String str1 = "Hello";
+		String str1 = "Hello";
 	String str2 = "World";
 	str1 = str1;
 
@@ -104,9 +132,11 @@ void main()
 
 	//String str3 = str1 + str2;	//CopyConstructor
 	String str3;
-	str3 = str1 + " "  + str2;		//CopyAssignment
+	str3 = str1 + " " + str2;		//CopyAssignment
 	//str3.print();
 	cout << str1 << endl;
 	cout << str2 << endl;
-	cout << str3 << endl;	//HelloWorld
+	cout << str3 << endl;	//HelloWorld  
+#endif // CAT_CHECK
+
 }
